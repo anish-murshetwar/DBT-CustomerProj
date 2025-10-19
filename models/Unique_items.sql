a55cd4dda7ed5928f0f09174b0753fc7cc50d336
Monarch-Anish <murshetwaranish@gmail.com>
2025-10-19 17:48:29 +0530

    This is the final chnages to the project

--- models/Unique_items.sql
+++ models/Unique_items.sql
@@ -1,6 +1,29 @@
-{{ dbt_utils.deduplicate(
-    relation=source('customers_raw', 'customers'),
-    partition_by='gender',
-    order_by="family_size desc",
-   )
-}}
+-- models/Unique_Items.sql
+-- Replaced dbt_utils.deduplicate with standard SQL to fix package error
+
+WITH ranked_items AS (
+
+    SELECT
+        *,
+        -- Assign a rank (rn) of 1 to the rows we want to keep
+        ROW_NUMBER() OVER (
+            PARTITION BY customer_id -- Unique key for the deduplication
+            ORDER BY family_size DESC, annual_income DESC
+            -- We keep the record with the largest family_size and then the largest income
+        ) AS rn
+    
+    FROM
+        {{ source('customers_raw', 'customers') }}
+
+)
+
+SELECT
+    customer_id,
+    gender,
+    annual_income,
+    family_size
+    -- Select all columns needed for downstream models
+FROM
+    ranked_items
+WHERE
+    rn = 1
